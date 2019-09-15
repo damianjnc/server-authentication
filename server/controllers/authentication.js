09349662d17bdb489e87d94a68a244b07ac2ef46
Damian <damianjjoniec@gmail.com>
2019-09-15 23:46:22 +0100

    first commit

---
+++ server/controllers/authentication.js
@@ -0,0 +1,47 @@
+const jwt = require('jwt-simple')
+const User = require('../models/user')
+const config = require('../config')
+
+function tokenForUser(user){
+    const timestamp = new Date().getTime()
+    return jwt.encode({sub:user.id, iat:timestamp}, config.secret)
+}
+
+exports.signup = function(req, res, next) {
+  const email = req.body.email
+  const password = req.body.password
+
+  if(!email || !password){
+      return res.status(422).send({error: 'You must provide email and password'})
+  }
+
+  // See if a user with the given address exists
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) {
+     return next(err)
+    }
+
+    // If a user does exist, return an error
+    if(existingUser){
+        return res.status(422).send({error: 'Email is in use'})
+    }
+
+    // If a user doen not exists, create and save user record
+        //--> create instance in the memory
+    const user = new User({
+        email: email,
+        password: password
+    })
+
+         //--save
+    user.save(function(err){
+        if(err){
+            return next(err)
+        }
+
+        // Respond to request indicating the user was created
+        res.json({token: tokenForUser(user)})
+    })
+
+  })
+}
