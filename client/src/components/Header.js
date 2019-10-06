4223408f699dccf647d891a0374bba192b3cc230
Damian <damianjjoniec@gmail.com>
2019-10-06 16:09:50 +0100

    adding redux thunk

---
+++ client/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div>
+      <Link to="/">Redux Auth</Link>
+      <Link to="/signup">Sign Up</Link>
+      <Link to="/signin">Sign In</Link>
+      <Link to="/">Sign Out</Link>
+      <Link to="/">Feature</Link>
+    
+    </div>
+  )
+}
+
+export default Header
---
+++ client/src/components/auth/Signup.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { reduxForm, Field } from 'redux-form'
+import { connect } from 'react-redux'
+import * as actions from '../../actions'
+
+const Signup = ({ handleSubmit }) => {
+  
+  const onSubmit = formProps => {
+    console.log(formProps)
+  }
+  
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <fieldset>
+        <label>Email</label>
+        <Field
+          name="email"
+          type="text"
+          component="input"
+          autoComplete="none"
+        />
+      </fieldset>
+      <fieldset>
+        <label>Password</label>
+        <Field
+          name="password"
+          type="password"
+          component="input"
+          autoComplete="none"
+        />
+      </fieldset>
+      <button>Sign up</button>
+    </form>
+  )
+}
+
+export default reduxForm({ form: 'signup' })(Signup)
